751320bcbed20b33eb5de5fa31271cfcc4dfc72f
Yuto Suzuki <yuutoo.advance@gmail.com>
2015-08-23 16:43:30 +0900

    add multi-perceptron

--- src/main/scala/Methods/SampleMatrix.scala
+++ src/main/scala/Methods/SampleMatrix.scala
@@ -1,6 +1,6 @@
 package Methods
 
-import breeze.linalg.DenseMatrix
+import breeze.linalg.{DenseVector, DenseMatrix}
 import breeze.stats.distributions.Gaussian
 
 /**
@@ -11,5 +11,12 @@ object SampleMatrix {
   def gaussianMatrix(row: Int, col: Int): DenseMatrix[Double] = {
     val normalGaussian = Gaussian(0, 1)
     DenseMatrix.fill(row, col)(normalGaussian.draw())
+  }
+}
+
+object SampleVector {
+  def gaussianVector(size: Int): DenseVector[Double] = {
+    val normalGaussian = Gaussian(0, 1)
+    DenseVector.fill(size)(normalGaussian.draw())
   }
 }
--- src/main/scala/MultiPerceptron.scala
+++ src/main/scala/MultiPerceptron.scala
@@ -2,6 +2,7 @@
  * Created by Yuto Suzuki on 15/08/23.
  */
 
+import Methods.{SampleMatrix, SampleVector}
 import breeze.linalg._
 import breeze.numerics.{ pow, sqrt }
 import breeze.plot.Figure
@@ -10,7 +11,14 @@ import breeze.plot._
 
 object MultiPerceptron {
 
+  def forward(input: DenseVector[Double], weight: DenseMatrix[Double], bias: DenseVector[Double]): DenseVector = {
+    input * weight + bias
+  }
+
   def main(args: Array[String]): Unit = {
-    
+    val dimension = 3
+
+    val startWeight = SampleMatrix.gaussianMatrix(dimension, dimension)
+    forward(startWeight,)
   }
 }
